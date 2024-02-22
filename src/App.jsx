b7a159a1560b3e1d93b225d465dc08a8cd1227d8
omarsalah25 <55956352+omarsalah25@users.noreply.github.com>
2024-02-22 11:04:10 +0200

    up

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,21 @@ function App() {
 
   return (
     <>
-      <div className=' container columns-1 lg:columns-2 mt-40 lg:ps-6 sm:ps-2'>
-
-        <div className='w-full	z-10'>
+      <div className='lg:columns-2 xl:mt-40 md:mt-30 mt-10 lg:ps-6 sm:ps-2'>
+        <div className='w-full col-span-1 xl:col-span-1 items-center'>
           <h1 className='text-white text-start  font-bold text-4xl'>Revolutionize Your Customer Relationships: Discover Our CRM Solutions Today!</h1>
-          <div className='columns-1 lg:columns-2 mt-40'>
+          <div className='lg:columns-2 md:columns-1 xl:mt-20 lg:mt-5 mt-8'>
             <div>
-              <p className='text-white'>loremaspidhasiudhasyg ddyasdgyashuidasj duyasbsdvgyfsdxht7qweysa k</p>
+              <p className='text-gray-200 opacity-50 text-start text-sm mb-1'>Experience the Power of Seamless Customer Engagement: Unlock Opportunities, Build Relationships, and Drive Growth with Our Intuitive CRM Platform.</p>
             </div>
             <div>
-              <p className='text-white'>loremaspidhasiudhasyg ddyasdgyashuidasj duyasbsdvgyfsdxht7qweysa k</p>
+              <p className='text-gray-200 opacity-50 text-start text-sm my-1'>Elevate Your Sales Process and Delight Customers: Streamline Workflows, Boost Productivity, and Enhance Satisfaction with Our Comprehensive Suite of CRM Solutions.</p>
 
             </div>
 
           </div>
         </div>
-        <div className='w-full   '>
+        <div className='w-full col-span-2 lg:col-span-2'>
           <video className='aspect-w-16 aspect-h-9' src='/home-vid2.mkv' autoPlay loop muted></video>
 
         </div>
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,15 +1,15 @@
 // // import { CustomFlowbiteTheme } from 'flowbite-react';
 
-// import Navbar from "../components/navbar/Navbar"
+import Navbar from "../components/navbar/Navbar"
 
-// const Layout = () => {
-//     return (
-//         <>
-//             <Navbar />
-//             {/* <div>{{ children }}</div> */}
+const Layout = () => {
+    return (
+        <>
+            <Navbar />
+            {/* <div>{{ children }}</div> */}
 
-//         </>
-//     )
-// }
+        </>
+    )
+}
 
-// export default Layout
+export default Layout
