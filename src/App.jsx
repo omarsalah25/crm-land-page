1678d56385dd9f28f206876ad61e71cd02915444
omarsalah25 <55956352+omarsalah25@users.noreply.github.com>
2024-02-22 15:13:19 +0200

    yp

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           </div>
         </div>
         <div className='w-full col-span-2 lg:col-span-2'>
-          <video className='aspect-w-16 aspect-h-9' src='/home-vid2.mp4' autoPlay loop muted playsInline ></video>
+          <video className='aspect-w-16 aspect-h-9' src='/home-vid2.mp4' autoPlay loop muted playsInline   ></video>
 
         </div>
       </div>
@@ -29,9 +29,29 @@ function App() {
         <div className='w-full col-span-1 xl:col-span-1 items-center'>
           <h1 className='text-white text-start font-bold text-4xl'>Data-Driven Insights for Informed Decisions. </h1>
         </div>
-        <div className='w-full col-span-2 bg-transparent lg:col-span-2 flex justify-end mt-24 shadow-gray-400 shadow-transparent '>
-          <img className='aspect-w-16 aspect-h-9 xl:skew-y-6 hover:skew-x-1 transition-all' src='/crmcrowd.PNG' autoPlay />
+        <div className='w-full col-span-2 bg-transparent lg:col-span-2 flex justify-end mt-24  '>
+          <img className='aspect-w-16 aspect-h-9 xl:skew-y-6 hover:skew-x-1 transition-all ease-in-out ' src='/crmcrowd.PNG' autoPlay />
 
+        </div>
+      </div>
+
+      <div className="banner-whitepatch">
+        <h2 className='text-white text-start font-bold text-4xl mt-10'>What's to love about our crm?</h2>
+        <div className="banner-info ">
+          <div className="love-bgn-box profile-img1">
+            <span className="first-bgn-sprite lov-icon1"></span>
+            <p className=''>Perfect sweet spot between overly simplistic spreadsheets and a highly complicated CRM</p>
+          </div>
+          <div className="love-bgn-box profile-img2">
+            <span className="first-bgn-sprite lov-icon2"></span>
+            <p className=''>Not just your first, but your CRM partner forever</p>
+          </div><div className="love-bgn-box profile-img1">
+            <span className="first-bgn-sprite lov-icon3"></span>
+            <p>Features you need at a price you can afford</p>
+          </div><div className="love-bgn-box profile-img2">
+            <span className="first-bgn-sprite lov-icon4"></span>
+            <p>The right blend of features to help you manage customer relationships and complement your marketing and sales efforts</p>
+          </div>
         </div>
       </div>
     </>
